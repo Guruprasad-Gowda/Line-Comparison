28be7c2c3d7968fc4cfc4219e3a1be3061cd0890
NamaskaraGuru <guru8953@gmail.com>
2022-12-24 08:01:20 +0530

    ADDED: code to display welcome message

---
+++ LineComparision.java
@@ -0,0 +1,9 @@
+package com.line_comparison;
+
+public class LineComparision {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparision problem");
+    }
+
+}
